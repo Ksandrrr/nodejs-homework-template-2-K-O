db74870947679044b4695c934c3e64e99f90bd43
Ksandrrr <105207003+Ksandrrr@users.noreply.github.com>
2023-04-08 13:03:06 +0300

    hw2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,17 @@
 const express = require('express')
-
+const controllersContacts = require("../../controllers/contacts")
 const router = express.Router()
+const schemas = require("../../schemas/contacts");
+const validateBody = require("../../utils/validateBody")
+router.get('/', controllersContacts.listContacts)
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:contactId', controllersContacts.getContactById)
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/', validateBody(schemas.contactAddSchema), controllersContacts.addContact)
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put('/:contactId', validateBody(schemas.contactAddSchema), controllersContacts.updateContactById)
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:contactId', controllersContacts.removeContact)
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
 
 module.exports = router
